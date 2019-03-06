a0562c5e6bc7fe3f27cb017905ee9c4fab428602
KField-Bucknell <kfield@bucknell.edu>
2019-03-06 08:17:18 -0500

    Added details to DEBrowser tutorial

--- DEBrowser/DEBrowser.R
+++ DEBrowser/DEBrowser.R
@@ -19,6 +19,7 @@ genefiles <- lapply(genefilelist, read_tsv)
 samplenames <- gsub("SARTools/S2_DRSC_CG8144_", "", genefilelist)
 samplenames <- gsub("SARTools/S2_DRSC_","", samplenames)
 samplenames <- gsub(".genes.tsv", "", samplenames)
+samplenames <- gsub("-","_", samplenames) # DEBrowser doesn't like -
 samplenames
 
 genefiles
@@ -61,3 +62,23 @@ colnames(transcripttable) %in% metadata$sample
 # 4. Start DEBrowser
 
 startDEBrowser()
+
+# 5. Data Exploration with DE Browser
+
+#1 Load the Count Data File and the Metadata File
+#2 Filter the data using CPM, CPM<1, in at least 3 samples (half of the samples)
+#3 Batch correct the data using TMM normalization, Combat correction method, condition as Treatment, batch as Batch
+#4 Visualize the PCA plot by changing Color field to "condition", and Shape field to "batch"
+#5 Go to DE Analysis and Add New Comparison
+#   Set the Condition 1 samples to the Untreated and Condition 2 to the RNAi samples
+#   Chose the appropriate DE Method, and leave the parameters on the default settings
+#   Go to Main Plots and explore the MA and Volcano plots
+
+# 6. Confirm that RNAi experiment worked 
+
+#1 Load in the gene results and metadata
+#2 Filter and batch correct as above
+#3 Run the appropriate DE Analysis 
+#4 Go to the Tables and sort by log10padjust search for FBgn0261552 - this is the *pasilla* gene
+
+
